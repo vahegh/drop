633413f62685716863e25ca35bc27309c3e2eb46
vahe ghazaryan <vahe.a.ghazaryan@ameriabank.am>
2025-12-09 15:53:26 +0400

    endpoints

--- routes/event.py
+++ routes/event.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from services.event import event_announcement
+from services.member_pass import update_apple_member_pass
 
 router = APIRouter(tags=['Event'], prefix="/api/event")
 
@@ -7,3 +8,8 @@ router = APIRouter(tags=['Event'], prefix="/api/event")
 # @router.post("/announce")
 # async def create_event_api(event_id):
 #     return await event_announcement(event_id)
+
+
+# @router.get("/update-member-pass")
+# async def update_member_pass():
+#     return await update_apple_member_pass()
