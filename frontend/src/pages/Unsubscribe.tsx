614284f86938b31958f6d21dd18266b08f8f441b
vahe ghazaryan <vahe.a.ghazaryan@ameriabank.am>
2026-03-13 00:25:25 +0400

    fix: remove unused useEffect import in Unsubscribe.tsx

--- frontend/src/pages/Unsubscribe.tsx
+++ frontend/src/pages/Unsubscribe.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import Layout from '../components/Layout'
 import Section from '../components/Section'
